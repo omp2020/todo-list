4264ce8c4fb4015c37c57fe82a271749a8daa1ac
omp2020 <ompatel.4981@gmail.com>
2021-03-15 16:52:49 +0530

    firebase integrations

---
+++ src/components/home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+
+class Home extends Component {
+  state = {}
+  render() {
+    return (
+      <>
+        <h1>Hello from Home</h1>
+      </>
+    )
+  }
+}
+
+export default Home
